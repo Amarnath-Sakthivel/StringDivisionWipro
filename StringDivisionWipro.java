5e0cc676179cffbdf55860748803fc853dc80716
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-12-19 14:45:01 +0530

    Add files via upload

---
+++ StringDivisionWipro.java
@@ -0,0 +1,25 @@
+package com.wipro.javapractice;
+
+public class StringDivisionWipro {
+	public static void main(String[] args) {
+		stringDivisionWipro("Have a nice day", 1, 3);
+	}
+
+	static void stringDivisionWipro(String input, int input1, int input2) {
+		String[] strArray = input.split(" ");
+		for (int i = 0; i < strArray.length; i++) {
+			if (i == input1 - 1 || i == input2 - 1) {
+				StringBuilder builder = new StringBuilder();
+				for (int j = 0; j < strArray[i].length() / 2; j++) {
+					builder.append(strArray[i].charAt(j));
+				}
+				for (int k = strArray[i].length() / 2; k < strArray[i].length(); k++) {
+					builder.append(strArray[i].charAt(k));
+				}
+				System.out.println(builder.reverse());
+			}
+
+		}
+	}
+
+}
